4b4c007b2d41c44bb90330a9bec5359e9827ddf1
U-BSD\397142 <397142@SHS-10D9C06DD.beaverton.k12.or.us>
2022-03-14 11:26:57 -0700

    Third Commit

--- hashtable.cpp
+++ hashtable.cpp
@@ -22,7 +22,7 @@ void addstudent(Node* hashtable[], int size, Student* s);
 void printstudent(Student* s);
 void print(Node* hashtable[], int size);
 bool check(Node* hashtable[], int size);
-void remove(Node* hashtable[], int size, int id);
+Node* remove(Node* hashtable[], int size, int id, bool needconfirm);
 Node** rehash(Node* hashtable[], int size);
 
 int main() {
@@ -30,7 +30,7 @@ int main() {
   char action[20];
   int deleteid;
   Node* hashtable [1600];
-  for (int i=0; i<size; i++) {
+  for (int i=0; i<1600; i++) {
     // create a dummy student as each head... makes this so much easier
     Student* s = new Student();
     s->id = i;
@@ -43,7 +43,7 @@ int main() {
   while (true) {
     cout << "What would you like to do? (ADD, RANDOM, PRINT, DELETE, QUIT)" << endl;
     cin >> action;
-    if (strcmp(action, "ADD") == 0) {
+    if (strcmp(action, "ADD") == 0 || strcmp(action, "add") == 0) {
       Student* s = new Student();
       cout << "First name: "; cin >> s->first;
       cout << "Last name: "; cin >> s->last;
@@ -52,33 +52,34 @@ int main() {
       cout << "\n";
       addstudent(hashtable, size, s);
     }
-    else if (strcmp(action, "PRINT") == 0) {
+    else if (strcmp(action, "PRINT") == 0 || strcmp(action, "print") == 0) {
       print(hashtable, size);
     }
-    else if (strcmp(action, "DELETE") == 0) {
-      cout << "Enter the ID number of the student you are deleting: ";
+    else if (strcmp(action, "DELETE") == 0 || strcmp(action, "delete") == 0) {
+      cout << "\nEnter the ID number of the student you are deleting: ";
       cin >> deleteid;
-      remove(hashtable, size, deleteid);
+      remove(hashtable, size, deleteid, true);
     }
-    else if (strcmp(action, "QUIT") == 0) {
+    else if (strcmp(action, "QUIT") == 0 || strcmp(action, "quit") == 0) {
       break;
     }
     else {
-      cout << "I don't recognize that input." << endl;
+      cout << "I don't recognize that input.\n" << endl;
     }
-    /*
     while (check(hashtable, size) == true) {
-      int newsize = 2*size;
-      Node* newtable [newsize];
       for (int i=0; i<size; i++) {
 	Node* current = hashtable[i]->next;
 	while (current != NULL) {
-	  addstudent(newtable, newsize, hashtable[i]->student);
+	  if ((current->student->id)%(2*size) != i) {
+	    Node* n = remove(hashtable, size, current->student->id, false);
+	    addstudent(hashtable, 2*size, n->student);
+	  }
+	  // addstudent(newtable, newsize, hashtable[i]->student);
 	  current = current->next;
 	}
       }
+      size = 2 * size;
     }
-    */
     
   }
 
@@ -156,25 +157,25 @@ bool check(Node* hashtable[], int size) {
   return b;
 }
 
-void remove(Node* hashtable[], int size, int id) {
+Node* remove(Node* hashtable[], int size, int id, bool needconfirm) {
   // remove a student
   int i = id % size;
   Node* previous = hashtable[i];
   Node* current = hashtable[i]->next;
   bool found = false;
-  char confirm;
+  char confirm = 'y';
   while (current != NULL) {
     if (current->student->id == id) {
-      cout << "I'm about to delete " << current->student->first << " " << current->student->last << ". Is this what you want to do? (y/n)" << endl;
-      cin >> confirm;
+      if (needconfirm) {cout << "I'm about to delete " << current->student->first << " " << current->student->last << ". Is this what you want to do? (y/n)" << endl;}
+      if (needconfirm) {cin >> confirm;}
       if (confirm == 'y' || confirm == 'Y') {
-	cout << "Deleting student." << endl;
+	if (needconfirm) {cout << "Deleting student.\n" << endl;}
 	previous->next = current->next;
-	delete current->student;
-	delete current;
+	return current;
       }
       else {
-	cout << "Ok. No student deleted." << endl;
+	cout << "Ok. No student deleted.\n" << endl;
+	return NULL;
       }
       found = true;
       break;
@@ -185,8 +186,9 @@ void remove(Node* hashtable[], int size, int id) {
     }
   }
   if (found == false) {
-    cout << "No student has that ID number. Nothing has been deleted." << endl;
+    cout << "No student has that ID number. Nothing has been deleted.\n" << endl;
   }
+  return NULL;
 }
 
 Node** rehash(Node* hashtable[], int size) {
